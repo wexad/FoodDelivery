a28f9256f01472f6fd49effe89f5bc066e0f5611
wexad <sherzodchoriyev747@gmail.com>
2024-09-18 16:01:19 +0500

    initial commit

---
+++ src/main/java/com/wexad/BurgerHub/BurgerHubApplication.java
@@ -0,0 +1,13 @@
+package com.wexad.BurgerHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgerHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wexad/BurgerHub/BurgerHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wexad.BurgerHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
